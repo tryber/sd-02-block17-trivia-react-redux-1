49c9a5c7a3afa8a623618bc6ff9de83ea6622a03
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-03-16 17:13:40 -0300

    Working ranking page

--- src/Store/storeTree.js
+++ src/Store/storeTree.js
@@ -22,7 +22,20 @@
 //   },
 //   ranking:
 //     [
-//       { name: nome - da - pessoa, score: 10, picture: url - da - foto - no - gravatar }
+//       { name: Johnatas, score: 10, picture: https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3 }
+//       { name: Pedro, score: 30, picture: https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3 }
+//       { name: Richard, score: 20, picture: https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3 }
 //     ],
 //   token: '',
 // }
+
+/* 
+const getLocalStor = 
+[ 
+    { name: 'Johnatas', score: 10, picture: 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3' },
+    { name: 'Pedro', score: 30, picture: 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3' },
+    { name: 'Richard', score: 20, picture: 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3' }
+]
+*/
+
+//  
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import '../style/Ranking.css';
 
+let getLocalStor = JSON.parse(localStorage.getItem('ranking'));
+getLocalStor.sort((a, b) => {
+  if (a.score > b.score) return -1;
+  if (a.score < b.score) return 1;
+  return 0;
+});
 const Ranking = () => (
   <div>
-    <h1>Página Ranking</h1>
+    <h1 className="ranking-title">Ranking</h1>
+    {getLocalStor.map((item) => {
+      return (
+        <div className="ranking-item" key={`${item.name}-${item.score}`}>
+          <img className="ranking-img" src={item.picture} />
+          <span className="ranking-text">{item.name} - {item.score} pontos</span>
+        </div>
+      )
+    })}
   </div>
 );
-
 
 export default Ranking;
