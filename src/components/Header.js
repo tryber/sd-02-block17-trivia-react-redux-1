31a64cfce0d095f8c14ffad209353214387e2c69
richardwellerson <richardwellerson@gmail.com>
2020-03-16 17:22:42 -0300

    Header feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Proptypes from 'prop-types';
 import '../style/Header.css'
 
 class Header extends React.Component {
@@ -14,20 +13,21 @@ class Header extends React.Component {
             alt={`${name} player gravatar`}
             src={picture}
           />
-          <p className="Header_player-name">Jogador: {name}</p>
+          <p
+            data-testid="header-player-name"
+            className="Header_player-name"
+          >
+            Jogador: {name}
+          </p>
         </div>
         <div className="Header_score">
-          <p className="Header_score-text">Pontos: {score}</p>
+          <p
+          data-testid="header-score"
+          className="Header_score-text">Pontos: {score}</p>
         </div>
       </div>
     );
   }
-}
-
-Header.propTypes = {
-  name: Proptypes.string,
-  srcAvatar: Proptypes.string,
-  points: Proptypes.string,
 }
 
 export default Header;
