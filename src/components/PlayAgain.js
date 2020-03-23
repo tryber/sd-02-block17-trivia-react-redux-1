a324501cec8258ccacae226faaa31421cda20971
richardwellerson <richardwellerson@gmail.com>
2020-03-23 17:29:52 -0300

    Code Climate

--- src/components/PlayAgain.js
+++ src/components/PlayAgain.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../style/Buttons.css';
 
-const PlayAgain = ({ buttonName }) => {
-  return (
-    <Link to="/questions">
-      <button
-        className="Button_play-again"
-        type="button"
-      >
-        {buttonName}
-      </button>
-    </Link>
-  );
-};
+const PlayAgain = ({ buttonName }) => (
+  <Link to="/questions">
+    <button
+      className="Button_play-again"
+      type="button"
+    >
+      {buttonName}
+    </button>
+  </Link>
+);
 
 export default PlayAgain;
+
+PlayAgain.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+}
--- src/components/ToRanking.js
+++ src/components/ToRanking.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../style/Buttons.css';
 
-const ToRanking = ({ buttonName }) => {
-  return (
-    <Link to="/ranking">
-      <button
-        className="Button_ranking"
-        type="button"
-      >
-        {buttonName}
-      </button>
-    </Link>
-  );
-};
+const ToRanking = ({ buttonName }) => (
+  <Link to="/ranking">
+    <button
+      className="Button_ranking"
+      type="button"
+    >
+      {buttonName}
+    </button>
+  </Link>
+);
 
 export default ToRanking;
+
+ToRanking.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+}
