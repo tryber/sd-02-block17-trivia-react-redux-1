8d5133382a9b572c90e0dfc030b959da68605d2a
richardwellerson <richardwellerson@gmail.com>
2020-03-26 17:15:06 -0300

    Tests Home | Trivia App

---
+++ src/Tests/services/renderWithRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) => ({
+  ...render(<Router history={history}>{ui}</Router>),
+  history,
+});
+
+export default renderWithRouter;
